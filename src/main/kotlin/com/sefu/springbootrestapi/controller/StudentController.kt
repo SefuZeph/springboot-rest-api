6943b3fe49d54cab028a5716b5232a5d313071b2
Zeph <zephaniahsefuoduori@gmail.com>
2023-05-09 20:50:09 +0300

    Use response entity to map request status

--- src/main/kotlin/com/sefu/springbootrestapi/controller/StudentController.kt
+++ src/main/kotlin/com/sefu/springbootrestapi/controller/StudentController.kt
@@ -2,6 +2,8 @@ package com.sefu.springbootrestapi.controller
 
 import com.sefu.springbootrestapi.bean.Student
 import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
@@ -16,25 +18,27 @@ class StudentController {
 
     // http://localhost:8080/student
     @GetMapping("student")
-    fun getStudent(): Student {
-        return Student(1, "John", "Doe")
+    fun getStudent(): ResponseEntity<Student> {
+        val student = Student(1, "John", "Doe")
+        return ResponseEntity.ok().header("custom-header", "sefu").body(student)
     }
 
     // http://localhost:8080/students
     @GetMapping("students")
-    fun getStudents(): MutableList<Student> {
+    fun getStudents(): ResponseEntity<MutableList<Student>> {
         val student = mutableListOf<Student>()
         student.add(Student(1, "John", "Doe"))
         student.add(Student(2, "Jane", "Doe"))
-        return student
+        return ResponseEntity.ok(student)
     }
 
     // http://localhost:8080/student/1/john
     @GetMapping("student/{id}/{first-name}")
     fun studentPathVariable(
         @PathVariable("id") studentId: Long, @PathVariable("first-name") studentName: String
-    ): Student {
-        return Student(studentId, studentName, "Doe")
+    ): ResponseEntity<Student> {
+        val student = Student(studentId, studentName, "Doe")
+        return ResponseEntity.ok(student)
     }
 
     // http://localhost:8080/students/query?id=3&name=zeph
@@ -45,16 +49,22 @@ class StudentController {
 
     // Create user
     @PostMapping("student/create")
-    @ResponseStatus(HttpStatus.CREATED)
-    fun studentCreate(@RequestBody student: Student):Student{
-        return student
+    fun studentCreate(@RequestBody student: Student): ResponseEntity<Student> {
+        return ResponseEntity(student, HttpStatus.CREATED)
     }
 
     // Update user
 
     @PutMapping("student/{id}/update")
-    fun updateStudent(@RequestBody student: Student,@PathVariable("id") studentId: Long):Student{
-        return student
+    fun updateStudent(@RequestBody student: Student, @PathVariable("id") studentId: Long): ResponseEntity<Student> {
+        return ResponseEntity.ok(student)
+    }
+
+    // Delete user
+
+    @DeleteMapping("student/{id}/delete")
+    fun deleteStudent(@PathVariable("id") studentId: Int): String {
+        return "Student successfully deleted!"
     }
 
 
